da6d3157f0e63feae2007a431ec11ee32cf6535d
Yaswanth-Nitta <yaswanthnitta@gmail.com>
2024-01-31 05:08:10 +0000

    Planets App

---
+++ src/components/PlanetItem/styledComponents.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components/macro'
+
+export const PlanetContainer = styled.div``
+
+export const Image = styled.img``
+
+export const Name = styled.h1``
+
+export const Description = styled.p``
---
+++ src/components/PlanetsSlider/styledComponents.js
@@ -0,0 +1,5 @@
+import styled from 'styled-components/macro'
+
+export const PlanetsAppContainer = styled.div``
+
+export const Heading = styled.h1``
